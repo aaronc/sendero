53885e135525e6a572054b38ffd0f74a271e892d
aaronc542 <devnull@localhost>
2007-08-31 06:39:08 +0000

    Added copyright notice.

--- sendero/xml/IForwardNodeIterator.d
+++ sendero/xml/IForwardNodeIterator.d
@@ -1,3 +1,8 @@
+/** 
+ * Copyright: Copyright (C) 2007 Aaron Craelius.  All rights reserved.
+ * Authors:   Aaron Craelius
+ */
+
 module sendero.xml.IForwardNodeIterator;
 
 enum XmlNodeType { Element, Data, CData, Comment, PI, Attribute };
