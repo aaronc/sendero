5533f9ab2e4c21a78db78549916490e026ffc5b6
aaronc542 <devnull@localhost>
2007-09-10 15:30:53 +0000

    Adding NodeType.d.

---
+++ sendero/xml/NodeType.d
@@ -0,0 +1,8 @@
+/** 
+ * Copyright: Copyright (C) 2007 Aaron Craelius.  All rights reserved.
+ * Authors:   Aaron Craelius
+ */
+
+module sendero.xml.NodeType;
+
+enum NodeType { Element, Data, CData, Comment, PI };
