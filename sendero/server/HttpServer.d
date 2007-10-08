ba9106e93df783aa456f3d872ad560a2dbf12ec4
rrichardson <devnull@localhost>
2007-10-08 03:52:38 +0000

    forgot some key files

---
+++ sendero/server/HttpServer.d
@@ -0,0 +1,33 @@
+/**
+ * Copyright: Copyright (C) 2007 Rick Richardson.  All rights reserved.
+ * License:   BSD Style
+ * Authors:   Rick Richardson
+ */
+
+module sendero.server.HttpServer;
+
+import tango.util.log.Log;
+import tango.util.log.Configurator;
+import tango.text.convert.Sprint;
+import tango.sys.linux.epoll;
+import tango.net.SocketConduit;
+import tango.io.model.IConduit;
+import tango.io.Stdout;
+import tango.io.Console;
+import tango.core.Exception;
+import tango.net.Socket;
+import sendero.server.AsyncServer;
+import sendero.server.HttpThread;
+import sendero.server.SenderoProvider;
+
+Logger logger;
+
+  void main()
+	{
+		auto sprint = new Sprint!(char);
+
+		auto provider = new SenderoProvider;
+		HttpThread.set_provider(provider); 
+		auto srv = new AsyncServer!(HttpThread);
+    srv.run();
+	}
---
+++ sendero/server/SenderoProvider.d
@@ -0,0 +1,20 @@
+/**
+ * Copyright: Copyright (C) 2007 Rick Richardson.  All rights reserved.
+ * License:   BSD Style
+ * Authors:   Rick Richardson
+ */
+
+module sendero.server.SenderoProvider;
+
+import mango.net.http.server.HttpProvider;
+import mango.net.http.server.HttpRequest;
+import mango.net.http.server.HttpResponse;
+
+
+class SenderoProvider : HttpProvider
+{
+	void service (HttpRequest request, HttpResponse response)
+	{
+
+	}
+}
