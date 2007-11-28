549ff8955a56643f689b3add696f3d8e95826c84
aaronc542 <devnull@localhost>
2007-11-28 17:56:51 +0000

    Deleting NodeType.d (use XmlNodeType.d instead).

--- sendero/xml/NodeType.d
+++
@@ -1,8 +0,0 @@
-/** 
- * Copyright: Copyright (C) 2007 Aaron Craelius.  All rights reserved.
- * Authors:   Aaron Craelius
- */
-
-module sendero.xml.NodeType;
-
-enum NodeType { Element, Data, CData, Comment, PI };
