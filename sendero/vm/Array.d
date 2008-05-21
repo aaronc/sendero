8def1db5953a57a9a6e77b9948b05b274e43935c
aaronc <aaronc+git@practivist.org>
2008-05-21 16:18:56 -0400

    Made sendero.vm.Object and sendero.vm.Array dependent on sendero_base.Array and Object

--- sendero/vm/Array.d
+++ sendero/vm/Array.d
@@ -1,35 +1,9 @@
 module sendero.vm.Array;
 
 import sendero_base.Core;
+import sendero_base.Array;
 
-class SenderoVMArray : IArray
+class SenderoVMArray : SenderoArray
 {
-	Var[] values;
-	
-	int opApply (int delegate (inout Var val) dg)
-	{
-		int res;
-		foreach(ref Var v; values)
-		{
-			if((res = dg(v)) != 0) break;
-		}
-		return res;
-	}
-	
-	Var opIndex(size_t i)
-	{
-		if(i > values.length) return Var();
-		return values[i];
-	}
-	
-	size_t length()
-	{
-		return values.length;
-	}
-	
-	void opCatAssign(Var v)
-	{
-		values ~= v;
-	}
 }
 alias SenderoVMArray Array;
