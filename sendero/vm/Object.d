918adc02e139ecee020676073b2e228010d7d7f2
aaronc <aaronc+git@practivist.org>
2008-05-21 16:14:09 -0400

    Moved sendero.vm.Object to sendero.vm.InheritingObject because in most cases we actually don't want object inheritance - only for variable scoping and classes mainly...

--- sendero/vm/Object.d
+++ sendero/vm/Object.d
@@ -1,45 +1,12 @@
 module sendero.vm.Object;
 
 import sendero_base.Core;
+import sendero_base.Object;
 
 import sendero.vm.Set;
 
-class SenderoVMObject : IObject
+class SenderoVMObject : SenderoObject
 {
-	Var[char[]] members;
-	
-	Var opIndex(char[] key)
-	{
-		auto pVar = key in members;
-		if(pVar) return *pVar;
-		else return Var();
-	}
-	
-	void opIndexAssign(Var var, char[] key)
-	{
-		members[key] = var;
-	}
-	
-	int opApply(int delegate(inout char[] key, inout Var val) dg)
-	{
-		int res;
-		foreach(char[] k, ref Var v; members)
-		{
-			if((res = dg(k, v)) != 0) break;
-			
-		}
-		return res;
-	}
-	
-	Var opCall(Var[] params, IObject ctxt)
-	{
-		return Var();
-	}
-	
-	void toString(Var[] flags, IObject ctxt, void delegate(char[]) write)
-	{
-	}
-	
 	SenderoVMObject add(X)(char[] key, X x)
 	{
 		Var v;
